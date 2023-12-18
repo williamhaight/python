5a945a0b3941a9a20ab10c7ce7152c442fa5ec28
williamhaight <60486629+williamhaight@users.noreply.github.com>
2023-12-17 19:01:10 -0700

    Created HelloWorld.py script as first project

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+five=5
+two=2
+def addVariables(num1, num2):
+    return num1+num2
+answer=addVariables(five,two)
+print(answer)
+if answer>10:
+    print("Greater than 10","=",answer)
+else:
+    print("Less than 10")
